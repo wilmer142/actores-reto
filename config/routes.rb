c2b45848a524b35e5aedd4bffc25014959a16297
wilmer142 <wilmercollege@gmail.com>
2019-01-27 19:54:40 -0500

    Solve challenge 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get '/actors/', to: 'actors#index'
+  #get '/actors/', to: 'actors#index'
+  #get ''
+
+  resources :actors, only: [:index, :new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
